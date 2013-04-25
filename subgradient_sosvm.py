125b0894313134abec659a234ee5f3b43896f5c7
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-04-25 11:44:06 +0200

    Few changes in subgradients. - Added ctrl+c handling. - Accuracy debug within the loop. - Parameter tuning (WIP, still no good results)

--- subgradient_sosvm.py
+++ subgradient_sosvm.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-from shogun.Structure	import LinearStructuredOutputMachine
+from shogun.Structure	import LinearStructuredOutputMachine, StructuredAccuracy
 
 class SubgradientSOSVM(LinearStructuredOutputMachine):
 	'''
 	TODO DOC
 	'''
-	def __init__(self, model, loss, labels, max_iterations=1000, learning_rate=0.001, C=1.0):
+	def __init__(self, model, loss, labels, max_iterations=100, learning_rate=0.01, C=1.0, debug_at_iteration=101):
 		LinearStructuredOutputMachine.__init__(self)
 		self.set_model(model)
 		self.set_loss(loss)
@@ -14,6 +14,7 @@ class SubgradientSOSVM(LinearStructuredOutputMachine):
 		self.max_iterations = max_iterations
 		self.learning_rate = learning_rate
 		self.C = C
+		self.debug_at_iteration = debug_at_iteration
 
 	'''
 	TODO DOC
@@ -25,24 +26,36 @@ class SubgradientSOSVM(LinearStructuredOutputMachine):
 		M = model.get_dim()
 		# Number of training examples
 		N = model.get_features().get_num_vectors()
+		# Accuracy evaluator
+		evaluator = StructuredAccuracy()
 
 		# Initialize the weight vector
 		w = np.zeros(M)
 
-		for t in xrange(self.max_iterations):
-			print 'Subgradient descent iteration #%d' % (t+1)
+		try:
+			for t in xrange(self.max_iterations):
+				print 'Subgradient descent iteration #%d' % (t+1)
 
-			# Initialize the cumulative subgradient
-			subgrad = np.zeros(M)
+				# Initialize the cumulative subgradient
+				subgrad = np.zeros(M)
 
-			for i in xrange(N):
-				# Loss-augmented inference for training example i
-				res = model.argmax(w, i, True)
-				# Subgradient for training example i
-				subgrad += res.psi_pred.get() - res.psi_truth.get()
+				for i in xrange(N):
+					# Loss-augmented inference for training example i
+					res = model.argmax(w, i, True)
+					# Subgradient for training example i
+					subgrad += res.psi_pred.get() - res.psi_truth.get()
 
-			# Update weight vector
-			w -= self.learning_rate*(w - 1.0*self.C/N*subgrad)
+				# Update weight vector
+				w -= self.learning_rate*(w - 1.0*self.C/N*subgrad)
+
+				if (t+1) % self.debug_at_iteration == 0:
+					# Check if the accuracy improves with the number of iterations
+					self.set_w(w)
+					predicted = self.apply()
+					accuracy = evaluator.evaluate(predicted, model.get_labels())
+					print '\t\t\tAccuracy: %.4f' % accuracy
+		except KeyboardInterrupt:
+			print '\t\t==== Execution stopped by user ===='
 
 		self.set_w(w)
 
@@ -50,7 +63,7 @@ class StochasticSubgradientSOSVM(LinearStructuredOutputMachine):
 	'''
 	TODO DOC
 	'''
-	def __init__(self, model, loss, labels, max_iterations=1000, learning_rate=0.001, C=1.0):
+	def __init__(self, model, loss, labels, max_iterations=1000, learning_rate=0.01, C=1.0, debug_at_iteration=1001):
 		LinearStructuredOutputMachine.__init__(self)
 		self.set_model(model)
 		self.set_loss(loss)
@@ -58,6 +71,7 @@ class StochasticSubgradientSOSVM(LinearStructuredOutputMachine):
 		self.max_iterations = max_iterations
 		self.learning_rate = learning_rate
 		self.C = C
+		self.debug_at_iteration = debug_at_iteration
 
 	'''
 	TODO DOC
@@ -69,20 +83,34 @@ class StochasticSubgradientSOSVM(LinearStructuredOutputMachine):
 		M = model.get_dim()
 		# Number of training examples
 		N = model.get_features().get_num_vectors()
+		# Accuracy evaluator
+		evaluator = StructuredAccuracy()
 
 		# Initialize the weight vector
 		w = np.zeros(M)
 
-		for t in xrange(self.max_iterations):
-			print 'Stochastic subgradient descent iteration #%d' % (t+1),
+		# Catch user interruption
+		try:
+			for t in xrange(self.max_iterations):
+				print 'Stochastic subgradient descent iteration #%d' % (t+1),
 
-			# Select randomly a training example
-			i = np.random.random_integers(0, N-1)
-			print 'training example %d chosen' % i
-			# Loss-augmented inference for training example i
-			res = model.argmax(w, i, True)
-			# Subgradient update
-			subgrad = res.psi_pred.get() - res.psi_truth.get()
-			w -= self.learning_rate*(w - 1.0*self.C/N*subgrad)
+				# Select randomly a training example
+				i = np.random.random_integers(0, N-1)
+				print 'training example %d chosen' % i
+				# Loss-augmented inference for training example i
+				res = model.argmax(w, i, True)
+				# Subgradient update
+				subgrad = res.psi_pred.get() - res.psi_truth.get()
+#				w -= self.learning_rate/(t+1.0)*(w - 1.0*self.C/N*subgrad)
+				w -= self.learning_rate*(w - self.C*subgrad)
+
+				if (t+1) % self.debug_at_iteration == 0:
+					# Check if the accuracy improves with the number of iterations
+					self.set_w(w)
+					predicted = self.apply()
+					accuracy = evaluator.evaluate(predicted, model.get_labels())
+					print '\t\t\tAccuracy: %.4f' % accuracy
+		except KeyboardInterrupt:
+			print '\t\t==== Execution stopped by user ===='
 
 		self.set_w(w)
